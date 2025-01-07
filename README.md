535d328c503063bb47b9d2883d98dba27ed0b28c
Ruzait Ahamed <111993066+ruzait@users.noreply.github.com>
2025-01-07 13:09:10 +0000

    First app

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # ReactJs
 
 ![download](https://github.com/user-attachments/assets/14760d50-9354-40e7-81d6-2401b2654c78)
+
+`npm create vite@latest`
---
+++ firstReactJSapp/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(11983)
+
+  return (
+    <>
+      <div>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>My First React Code</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+      <p className="read-the-docs">
+        Zainudeen Ruzaid Ahamed
+      </p>
+    </>
+  )
+}
+
+export default App
